c99b5fe167f4c14c7e7ec6ed4778cbe646a2cc8f
Evan Chu <evanchu@dhcp-93-223.cu-internet.carleton.ca>
2019-02-16 15:23:21 -0500

    added genClassID() method, which generates a 8 character random alphanumeric

--- grader/grades/views.py
+++ grader/grades/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 def homepage(request):
     return render(request,"projects/main.html")
+
+def genClassID():
+    return ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') for i in range(8))
+
+
+
+print(genClassID())
