9e561f9a408393e738cb0fc7f0048b1b2ae0b541
YoussefZayed <yzaye1@ocdsb.ca>
2019-02-16 14:47:02 -0500

    Html and css added

--- grader/grader/settings.py
+++ grader/grader/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grades.apps.GradesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
