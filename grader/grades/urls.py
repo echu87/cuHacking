fc08ccfc11312fcbb484db4f15cc9dfce94690b9
Matthew Hobbs <matt.david.hobbs@gmail.com>
2019-02-16 20:21:46 -0500

    Added Classroom Page.

--- grader/grades/urls.py
+++ grader/grades/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('classroom', views.classroom, name='classroom')
 ]
--- grader/grades/views.py
+++ grader/grades/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import Classroom, Task
 
 # Create your views here.
 def homepage(request):
-    return render(request,"projects/main.html")
+    if request.user.is_authenticated:
+        return render(request,"projects/main.html",{'first_name':request.user.first_name})
+    else:
+        return render(request,"projects/main.html",{'first_name':None})
+
+def classroom(request):
+    return render(request,"projects/classroom.html",{})
 
 
