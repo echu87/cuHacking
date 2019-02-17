29d4a71c32cc06b2139e2b6a6282febad170332a
Matthew Hobbs <matt.david.hobbs@gmail.com>
2019-02-16 21:32:08 -0500

    Saved Code.

---
+++ grader/grades/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class ClassForm(forms.Form):
+    teacher = forms.CharField(label='Teacher', max_length=100)
+    subject = forms.CharField(label='Subject', max_length=100)
--- grader/grades/views.py
+++ grader/grades/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Classroom, Task
 
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import ClassForm
+
 # Create your views here.
 def homepage(request):
     if request.user.is_authenticated:
@@ -11,6 +16,15 @@ def homepage(request):
         return render(request,"projects/main.html",{'first_name':None})
 
 def classroom(request):
-    return render(request,"projects/classroom.html",{})
+    if request.method == 'POST':
+        form = ClassForm(request.POST)
+        if form.is_valid():
+            print(form)
+            form = ClassForm()
+    else:
+        form = ClassForm()
 
-
+    if request.user.is_authenticated:
+        return render(request,"projects/classroom.html",{'first_name':None,'form':form})
+    else:
+        return render(request,"projects/classroom.html",{'form':form})
