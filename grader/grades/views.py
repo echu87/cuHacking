568db767e4bb8a2e56a7056db21ef08689609c03
Ojas Bhatia <ojasbhatia24@gmail.com>
2019-02-17 03:31:19 -0500

    colours added to spreadsheet

--- grader/grades/views.py
+++ grader/grades/views.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup as bs
 import requests
 # Create your views here.
 def homepage(request):
-    context = {"name":findResources()}
+    context = { "video":findVideo()}
     return render(request,"projects/main.html",context)
 
-def findResources():
+def findVideo():
     mozhdr = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'}
 
     requests.get("https://www.youtube.com", headers = mozhdr)
@@ -22,6 +22,7 @@ def findResources():
     sb_get.content
 
     soupeddata = bs(sb_get.content, "html.parser")
+    
     yt_links = soupeddata.find_all("a", class_ = "yt-uix-tile-link")
 
     for x in yt_links:
@@ -29,8 +30,10 @@ def findResources():
         yt_title = x.get("title")
     
     link = scrape_url + yt_href
+    title = yt_title
     
-    
-    return link
+    return [link, title]
+
+
 
 
